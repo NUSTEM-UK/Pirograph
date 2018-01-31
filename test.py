a0a71e83e6dcc88e9e66e365dfaf0b3be077de9b
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2018-01-31 13:37:20 +0000

    Minor changes, diagrams

--- test.py
+++ test.py
@@ -7,27 +7,45 @@ Note the import form: `import skutterzero` doesn't do what we intend.
 from skutterzero import Skutter
 from time import sleep
 
-MY_SKUTTER = Skutter("D10")
+boris = Skutter("D08")
 
-MY_MAC = MY_SKUTTER.getMac()
+MY_MAC = boris.getMac()
 print(MY_MAC)
 
-MY_SKUTTER.setBrightness(0)
-MY_SKUTTER.LEDcolour(90)
-MY_SKUTTER.setBrightness(100)
+boris.setBrightness(0)
+boris.LEDcolour(90)
+boris.setBrightness(100)
 sleep(0.5)
-MY_SKUTTER.LEDcolour(120)
-MY_SKUTTER.setBrightness(255)
+boris.LEDcolour(120)
+boris.setBrightness(255)
 
 delay = 1
-MY_SKUTTER.servoSpeed(0.0)
+
+boris.servoSpeed(90)
+boris.servo2speed(90)
 sleep(delay)
-MY_SKUTTER.servoSpeed(90.0)
+
+boris.servoSpeed(120)
 sleep(delay)
-MY_SKUTTER.servo2speed(0.0)
+boris.servoSpeed(90.0)
 sleep(delay)
-MY_SKUTTER.servo2speed(180.0)
+boris.servoSpeed(75)
+sleep(delay)
+boris.servoSpeed(90)
+sleep(delay)
+
+boris.LEDcolour(0)
+boris.setBrightness(50)
+
+# boris.servoSpeed(0.0)
+# sleep(delay)
+# boris.servoSpeed(90.0)
+# sleep(delay)
+# boris.servo2speed(0.0)
+# sleep(delay)
+# boris.servo2speed(180.0)
 
 # for x in range(25):
-#     MY_SKUTTER.setBrightness(x * 10)
+#     boris.setBrightness(x * 10)
 #     sleep(0.2)
+
