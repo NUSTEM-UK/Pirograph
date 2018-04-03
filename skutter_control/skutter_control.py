e49d0a786bfa160deef69ca921b98a3eeda87cd5
Jonathan Sanderson <lists@quernstone.com>
2018-04-03 15:18:33 +0100

    First MQTT success: Bootstrap button wired to MQTT message.

--- skutter_control/skutter_control.py
+++ skutter_control/skutter_control.py
@@ -8,18 +8,27 @@ from flask import redirect
 from flask import render_template
 from flask import url_for
 from flask import request
+from flask import flash
 from skutterzero import Skutter
 
 app = Flask(__name__)
+app.secret_key = '2B4C0s8ObsIuL6pxvbfJaTm+MJcfvLKSw9IzTNlr1T5pYJZ1kSzQz'
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/derek")
+def derek():
+    return render_template("derek.html")
+
 @app.route("/send", methods=["POST"])
 def send():
-    
-    return
+    servo1speed = request.form.get("servo1speed")
+    flash_message = "Servo1Speed: " + servo1speed
+    derek.servo1speed(servo1speed)
+    flash(flash_message)
+    return redirect(url_for('derek'))
 
 if __name__ == '__main__':
     derek = Skutter("D07")
