657bcdae13232659251cde80d780aa6bd09c8459
Joe Shimwell <spgk4@c15000173.campus.unn.ac.uk>
2018-04-18 09:33:07 +0100

    Slight adjustments to make for easier testing - mqtt set correctly and pixel numbers

--- skutter/skutter.ino
+++ skutter/skutter.ino
@@ -35,7 +35,7 @@ char subsTargetArray[60];
 #define PIN_LED_RED D3
 #define PIN_PIXEL D1
 
-#define PIXEL_COUNT 1
+#define PIXEL_COUNT 9
 #define SERVO_COUNT 2
 
 
--- skutterzero.py
+++ skutterzero.py
@@ -14,8 +14,8 @@ from skutter_mac import MACS
 mqttc = mqtt.Client()
 
 # TODO: expose interface for configuring the MQTT broker address & port
-# mqtt_server = "10.0.1.3"
-mqtt_server = "localhost"
+mqtt_server = "10.0.1.3"
+#mqtt_server = "localhost"
 # mqtt_server = "192.168.0.31"
 mqtt_port = 1883
 # TODO: expose interface for configuring the MQTT channel root
