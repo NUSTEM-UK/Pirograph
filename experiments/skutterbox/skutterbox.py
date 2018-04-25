d0e4f02f19e496044aa87e97a6efcb07067c900c
llewmihs <joeshimwell@gmail.com>
2018-04-25 21:13:46 +0100

    Complete, but untested, box controller code .

--- experiments/skutterbox/skutterbox.py
+++ experiments/skutterbox/skutterbox.py
@@ -20,11 +20,11 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # define the Analog input channels
-servo1speed = 0
-servo2angle = 1
-startColour = 2
-endColour = 3
-transitionTime = 4
+s1speed = 0
+s2angle = 1
+sColour = 2
+eColour = 3
+tTime = 4
 
 # neo strip array
 LEDS = [7]
@@ -35,65 +35,93 @@ sendButton = Button(3)
 
 # the state holders so that we can act on button presses
 sendToggle = 1
+stateToggle = 1
 
 #sendButton.is_pressed == True (State A), False (State B)
 stateToggle = sendButton.is_pressed
 
 
 # arrays to hold the current values for state A and B
-stateAvals = [0,0,0,0,0]
-stateBvals = [0,0,0,0,0]
+stateAvals = [0,0,0,0]
+stateBvals = [0,0,0,0]
+tranTime = 5
 
 # choose your skutter carefully
+box = Skutter("??")
+
+# setup for the Neopixels
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-# setup for the Neopixels
+def allSend():
+    # grab the transition time value and map
+    tranTime = mapper((MCP3008(channel=4, device=0),4)
+    # send the data currently in the arrays, and reset the toggle to false
+    # first motor speed
+    box.servo1speedA(stateAvals[0])
+    box.servo1speedB(stateBvals[0])
+    #second motor angles
+    box.servo2positionA(stateAvals[1])
+    box.servo2positionB(stateBvals[1])
+    # start LED hues
+    box.LEDstartHueA(stateAvals[2])
+    box.LEDstartHueB(stateBvals[2])
+    # end LED hues
+    box.LEDendHueA(stateAvals[3])
+    box.LEDendHueB(stateBvals[3])
+    # transition time
+    box.transitionTime(tranTime)
+
+#function to map an analog input (0-100, to the relevant output)
+def mapper(x, analogType):
+    if analogType == 0:
+        # speed input
+        mappedVal = int((x - 0) * (1000 - 0) / (1 - 0) + 0)
+    elif analogType == 1:
+        # angle input
+        mappedVal = int((x - 0) * (360 - 0) / (1 - 0) + 0)
+    elif analogType == 2 or analogType == 3:
+        # colour input
+        mappedVal = int((x - 0) * (255 - 0) / (1 - 0) + 0)
+    else:
+        # time input
+        mappedVal = int((x - 0) * (30 - 0) / (1 - 0) + 0)
+    return mappedVal
 
 while True:
     # check if the send button has been pressed, if yes send the data we have
     if sendButton.is_pressed and sendToggle == 1:
-        # send the data currently in the arrays, and reset the toggle to false
-
+        allSend()
         # set the toggle to wait for the next press
-        sendToggle == 0
+        sendToggle = 0
     elif sendButton.is_pressed is False and sendToggle == 0:
-        # send the data currently in the arrays, and reset the toggle to false
-
+        allSend
         # set the toggle to wait for the next press
-        sendToggle == 1
-    if stateButton.is_pressed:
-        for i in range (5):
+        sendToggle = 1
+
+    if stateButton.is_pressed and stateToggle ==1:
+        stateToggle = 0
+        #write the values for inputs 0-3 to the state A array
+        for i in range (4):
             stateAvals[i] = mapper((MCP3008(channel=i, device=0),i)
-            stColour = colorsys.hsv_to_rgb(stateAvals[startColour],255,255)
-            enColour = colorsys.hsv_to_rgb(stateAvals[endColour],255,255)
-    else:
-        for i in range (5):
+    elif stateButton.is_pressed is False and stateToggle ==0:
+        stateToggle = 0
+        #write the values for inputs 0-3 to the state B array
+        for i in range (4):
             stateBvals[i] = mapper((MCP3008(channel=i, device=0),i)
-            stColour = colorsys.hsv_to_rgb(stateBvals[startColour],255,255)
-            enColour = colorsys.hsv_to_rgb(stateBvals[endColour],255,255)
-    # take the colour inputs and map them from HSV to RGB
-    stColour = colorsys.hsv_to_rgb()
+
+    # loop the colour display
+    startColour = int(mapper((MCP3008(channel=sColour, device=0),sColour))
+    endColour = int(mapper((MCP3008(channel=eColour, device=0),eColour))
+
+    # map the start and end colours to create a gradient across the 8 pixel strip
+    for i in range(len(LEDS)):
+        gradColour = (endColour - startColour)/(7 - 0)*(i - 0) + startColour
+        gradRGB = colorsys.hsv_to_rgb(gradColour,255,255)
+        strip.setPixelColor(i, gradRGB)
+    strip.show()
 
 
-# function to map an analog input (0-100, to the relevant output)
-def mapper (x, analogType):
-    if analogType == 0:
-        # speed input
-        mappedVal = (x - 0) * (1000 - 0) / (1 - 0) + 0
-    elif analogType == 1:
-        # angle input
-        mappedVal = (x - 0) * (360 - 0) / (1 - 0) + 0
-    elif analogType == 2 or analogType == 3:
-        # colour input
-        mappedVal = (x - 0) * (255 - 0) / (1 - 0) + 0
-    else:
-        # time input
-        mappedVal = (x - 0) * (30 - 0) / (1 - 0) + 0
-    return mappedVal
-            
-def colourMapper ():
-    for i in range len(LEDS):
-        j = int((x - 0) * (enColour[0]-stColour[0]) / (len(LEDS) - 0) + stColour[0])
-        LEDS[i] = (j,255,255)
+
+
